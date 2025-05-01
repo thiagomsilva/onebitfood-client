f5564d963b8e6c1c36b7288b028990b0e1139394
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2025-05-01 19:06:00 -0300

    Adiciona componente Typewriter: implementa animação de texto na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { Button, Row, Col } from "react-bootstrap"
 import Link from "next/link";
 import { FaCrosshairs } from "react-icons/fa";
+import Typewriter from "../components/Typewriter";
 
 export default function Home() {
   return (
     <Row className="mt-8 justify-content-center">
       <Col md={7} xs={12} className="text-center">
         <h1 className="fw-bolder text-custom-gray-darker mb-5 lh-base display-5">
-          Comida saudável e saborosa direto na sua casa.
+          <Typewriter text="Comida saudável e saborosa direto na sua casa." />
         </h1>
         <Link href="/restaurants">
           <Button variant="custom-red" size="lg" className="text-white">
